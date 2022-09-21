9bbead0a23dec3982afcbc0b29dbf4a1de5bb8d3
Fatih Nejat <fatih.nejatyilmaz@gmail.com>
2022-09-21 22:06:59 +0300

    Calculation functions created

--- checkout.js
+++ checkout.js
@@ -1,37 +1,77 @@
-const taxRate = 0.18;
-const shippingPrice = 15;
-const shippingFreePrice = 300;
+// const taxRate = 0.18;
+// const shippingPrice = 15;
+// const shippingFreePrice = 300;
 
-window.addEventListener("load", () => {
-  // set items to Local Srtorage and Session
-  localStorage.setItem("taxRate", taxRate);
-  localStorage.setItem("shippingPrice", shippingPrice);
-  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+window.addEventListener("load", ()=>{
+  //set items to LocalStorage
+  // localStorage.setItem("taxRate", taxRate);
+  // localStorage.setItem("shippingPrice", shippingPrice);
+  // localStorage.setItem("shippingFreePrice", shippingFreePrice);
 
-  sessionStorage.setItem("taxRate", taxRate);
-  sessionStorage.setItem("shippingPrice", shippingPrice);
-  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+   //set items to sessionStorage
+  //  sessionStorage.setItem("taxRate", taxRate);
+  //  sessionStorage.setItem("shippingPrice", shippingPrice);
+  //  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const productsDiv = document.querySelector(".products");
-productsDiv.addEventListener("click", (e) => {
-  if (e.target.className == "fa-solid fa-minus") {
-    if(e.target.parentElement.querySelector(".quantity").innerText >1 ){
-      e.target.parentElement.querySelector(".quantity").innerText --;
-    }else{
-      if(confirm("Product will be removed ?") ){
-        e.target.parentElement.parentElement.parentElement.remove();
-
+//Capturing vs. Bubbling
+productsDiv.addEventListener("click", (event)=>{
+  if(event.target.className == "fa-solid fa-minus"){
+      //console.log("minus btn is clicked!");
+      if(event.target.parentElement.querySelector(".quantity").innerText > 1){
+          event.target.parentElement.querySelector(".quantity").innerText--;
+          calculateProductPrice(event.target);
+          calculateCartPrice();
       }
-    }
-    
-    
-  } else if (e.target.classList.contains("fa-plus")) {
-    e.target.previousElementSibling.innerText ++;
-    
-  } else if (e.target.className == "remove-product") {
-    
-  } else {
-    
+      else{
+          if(confirm("Product will be removed???")){
+              //remove
+              event.target.parentElement.parentElement.parentElement.remove();
+              calculateCartPrice();
+          }
+      }
+  }
+  else if(event.target.classList.contains("fa-plus")){
+      //console.log("plus btn is clicked!");
+      event.target.previousElementSibling.innerText++;
+      calculateProductPrice(event.target);
+      calculateCartPrice();
+  }
+  else if(event.target.className == "remove-product"){
+      //console.log("remove btn is clicked!");
+      event.target.parentElement.parentElement.parentElement.remove();
+      calculateCartPrice();
+  }
+  else{
+      //console.log("other element is clicked!");
   }
 });
+
+const calculateProductPrice = (btn) =>{
+  const productInfoDiv = btn.parentElement.parentElement;
+  //console.log(productInfoDiv);
+  const price = productInfoDiv.querySelector(".product-price strong").innerText;
+  const quantity = productInfoDiv.querySelector(".quantity").innerText;
+  const productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+  productTotalDiv.innerText = (price * quantity).toFixed(2);
+  //alert(quantity);
+
+}
+
+const calculateCartPrice = () =>{
+  const productsTotalPricesDivs = document.querySelectorAll(".product-line-price");
+  //foreach ==> NodeList, Array
+  //const productsTotalPricesDivs = [...document.getElementsByClassName("product-line-price")];
+
+  let subtotal = 0;
+  productsTotalPricesDivs.forEach(div =>{
+      subtotal += parseFloat(div.innerText);
+  });
+  //console.log(subtotal);
+  const taxPrice = subtotal * localStorage.getItem("taxRate");
+
+  const shippingPrice = (subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice") ? localStorage.getItem("shippingPrice") : 0)
+
+  console.log(shippingPrice);
+}
